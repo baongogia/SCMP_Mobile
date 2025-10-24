70d439216b5aebf97067393c798d1fb6e447b600
Gia Bảo <NgoBao@MacBook-Pro-cua-Bao.local>
2025-10-24 21:49:00 +0700

    Update

--- src/screens/member/home/PaymentHistory/PaymentHistoryScreen.tsx
+++ src/screens/member/home/PaymentHistory/PaymentHistoryScreen.tsx
@@ -83,6 +83,7 @@ export default function PaymentHistoryScreen() {
   const getStatusText = (status: string[]) => {
     if (status.includes("paid")) return "Đã thanh toán";
     if (status.includes("expired")) return "Đã hết hạn";
+    if (status.includes("pending")) return "Đang chờ thanh toán";
     if (status.includes("refund")) return "Đã hoàn trả";
     return "Không xác định";
   };
