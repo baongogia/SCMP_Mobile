0e651a5d9bbed8d3eb2c7d83d6ae061cf1619bd5
Gia Bảo <NgoBao@MacBook-Pro-cua-Bao.local>
2025-11-28 09:54:18 +0700

    Update animation

--- src/components/layout/schedule/TodayScheduleSection.tsx
+++ src/components/layout/schedule/TodayScheduleSection.tsx
@@ -409,7 +409,7 @@ const styles = StyleSheet.create({
   },
   overlay: {
     ...StyleSheet.absoluteFillObject,
-    backgroundColor: "rgba(0, 0, 0, 0.15)",
+    backgroundColor: "rgba(0, 0, 0, 0.05)",
     // "rgba(0, 62, 159, 0.75)",
     borderRadius: 14,
   },
